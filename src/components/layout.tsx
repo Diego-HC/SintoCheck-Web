c0a357576f7c42d9b6edb7326a2c07d5ab884ffb
Diego Hernandez <A00834015@tec.mx>
2023-11-25 23:20:12 -0600

    added logo as page icon

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from "react";
+import Head from "next/head";
 
 type LayoutProps = {
   children: ReactNode;
@@ -6,9 +7,17 @@ type LayoutProps = {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="">
-      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-100 text-lg min-w-full">{children}</main>
-    </div>
+    <>
+      <Head>
+        <title>SintoCheck</title>
+        <link rel="icon" href="/favicon.jpeg" />
+      </Head>
+      <div className="">
+        <main className="flex min-h-screen min-w-full flex-col items-center justify-center bg-slate-100 text-lg">
+          {children}
+        </main>
+      </div>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,21 @@
-import Head from "next/head";
 import Link from "next/link";
 import Layout from "sintocheck/components/layout";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>SintoCheck</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {/* login page */}
-      <Layout>
-        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-          <h1 className="text-6xl font-bold">
-            Welcome to <strong>SintoCheck!</strong>
-          </h1>
+    <Layout>
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to <strong>SintoCheck!</strong>
+        </h1>
 
-          <p className="mt-3 text-2xl">
-            Get started by{" "}
-            <Link className="underline text-blue-500" href="/dashboard">
-              logging in
-            </Link>
-          </p>
-        </main>
-      </Layout>
-    </>
+        <p className="mt-3 text-2xl">
+          Get started by{" "}
+          <Link className="text-blue-500 underline" href="/dashboard">
+            logging in
+          </Link>
+        </p>
+      </main>
+    </Layout>
   );
 }
